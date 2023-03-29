2bde16f03b50fee2ae099ad0616f091860eca635
Visage <82640121+AaaaaaaaAaaaaaaaaaaaaaaaaa4217@users.noreply.github.com>
2023-03-29 14:39:29 +0300

    123

--- 123.py
+++ 123.py
@@ -7,6 +7,8 @@ for example, reciving a news feed, but there are also functions outside VK, for 
 import time
 import os
 import requests
+import numpy as np
+
 
 import telebot
 import urllib
@@ -19,6 +21,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import vk_api
+import cv2 
+from PIL import ImageFont, ImageDraw, Image
 
 import config
 
@@ -29,6 +33,9 @@ vk_session = vk_api.VkApi(token=config.VkToken)
 API_KEY = config.GoogleAPIKey
 SEARCH_ENGINE_ID = config.EngineID
 start_from = ''
+font_path = r"C:\Windows\Fonts\ARIALN.TTF"
+font = ImageFont.truetype(font_path, 60)
+
 
 # Chrome options
 chrome_options = Options()
@@ -47,7 +54,7 @@ def hello_msg(message):
     keyboard = ReplyKeyboardMarkup()
     button1 = KeyboardButton('Новости')
     button2 = KeyboardButton('Чат')
-    keyboard.add(button1, button2)
+    keyboard.add(button1)
     """
     { hello mail }
 
@@ -95,7 +102,6 @@ def handle_search(message):
 
 # commands handler
 
-
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     if message.text == 'Новости':
@@ -104,7 +110,7 @@ def echo_all(message):
         vk = vkapp.newsfeed.get(count=21, start_from=start_from)
         # parse vk-newsfeed
         for feed in vk['items']:
-            time.sleep(0.5)
+            time.sleep(1)
             # deafault post
             if feed['type'] == 'post':
                 for attach in feed['attachments']:
@@ -156,89 +162,6 @@ def echo_all(message):
                         pass
         # eliminate duplicates in feed
         start_from = vk['next_from']
-
-    elif message.text == 'Чат':
-        keyboard = ReplyKeyboardMarkup()
-
-        getmsgBtn = KeyboardButton(
-            'Получить последние сообщения')
-        keyboard.add(getmsgBtn)
-        sendmsgBtn = KeyboardButton('Переписка')
-        keyboard.add(sendmsgBtn)
-
-        bot.send_message(chat_id=message.chat.id,
-                         text='Выберите кнопку:',
-                         reply_markup=keyboard)
-
-    elif message.text == 'Получить последние сообщения':
-        vk = vk_session.get_api()
-        # get chat id for bot
-        chat_id = message.chat.id
-        # getting a list of dialogs
-        dialogs = vk.messages.getConversations(count=5)['items']
-        # processing each dialogue
-        for dialog in dialogs:
-            if dialog['conversation']['peer']['type'] == 'user':
-                peer_id = dialog['conversation']['peer']['id']
-            else:
-                peer_id = 2000000000 + dialog['conversation']['peer']['id']
-            # get user info
-            user_info = vk.users.get(user_ids=peer_id)
-            if user_info:
-                user_info = user_info[0]
-                user_name = user_info['first_name']
-                user_last = user_info['last_name']
-            else:
-                user_name = 'unknown'
-                user_last = 'unknown'
-
-            # get last five messages
-            messages = vk.messages.getHistory(
-                peer_id=peer_id, count=5)['items']
-
-            """
-            generating a message to send
-            format First name Last name: text
-            """
-            for message in reversed(messages):
-                sender_id = message['from_id']
-                sender_info = vk.users.get(user_ids=sender_id)[0]
-                sender_name = sender_info['first_name']
-                sender_last = sender_info['last_name']
-
-                text = message['text']
-
-                message_dict = {
-                    "sender_name": sender_name,
-                    "sender_last": sender_last,
-                    "text": text,
-                }
-                formatted_message = f"{sender_name} {sender_last}: {text}"
-
-                # send message
-                if text != "":
-                    bot.send_message(chat_id, formatted_message)
-                else:
-                    pass
-            # transition to a new dialogue
-            bot.send_message(chat_id, '...')
-
-        """
-        test function. Get video from vk-newsfeed
-        """
-
-        # elif attach['type'] == 'video':
-        #     url = f"https://vk.com/{vk.groups.getById(group_id=attach['video']['owner_id']*-1)[0]['screen_name']}?z=video{attach['video']['owner_id']}_{attach['video']['id']}"
-        #     if feed['text'] != "":
-        #         ydl_opts
-        #         = {}
-        #         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        #             downloaded = ydl.download([url])
-
-        #         bot.send_video(message.chat.id, vid, caption=feed['text'])
-        #     else:
-        #         bot.send_video(message.chat.id, vid)
-
 
 # start bot
 bot.infinity_polling()
